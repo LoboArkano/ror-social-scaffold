b08e24515e4c3dcda75baabb0b27857769e60e37
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-22 14:09:43 -0400

    Update ERD diagram. Add linters and fix scss errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
-  def menu_link_to(link_text, link_path)
+  def menu_link_to(link_text, link_path, link_class)
     class_name = current_page?(link_path) ? 'menu-item active' : 'menu-item'
 
     content_tag(:div, class: class_name) do
-      link_to link_text, link_path
+      link_to link_text, link_path, link_class
     end
   end
 
   def like_or_dislike_btn(post)
     like = Like.find_by(post: post, user: current_user)
     if like
-      link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete)
+      link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete, class: "like-link")
     else
-      link_to('Like!', post_likes_path(post_id: post.id), method: :post)
+      link_to('Like!', post_likes_path(post_id: post.id), method: :post, class: "like-link")
     end
   end
 end
